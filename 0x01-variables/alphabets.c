2740915190e784626234305b9d2e30dec4406d01
strongvillecode <strongvilleengineering@gmail.com>
2022-09-17 16:26:36 +0000

    tt

---
+++ 0x01-variables/alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
